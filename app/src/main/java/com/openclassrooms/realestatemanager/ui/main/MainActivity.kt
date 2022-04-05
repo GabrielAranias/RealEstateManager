3d62c5fe3df6b2f14c00abd02760bfa9d48a7c8d
Gabriel Aranias <gabriel.aranias@hotmail.fr>
2022-04-05 20:30:11 +0200

    Toolbar x AddActivity prep

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/main/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/main/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.openclassrooms.realestatemanager.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.ActivityMainBinding
+import com.openclassrooms.realestatemanager.ui.add.AddActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +17,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.toolbar_add -> {
+                startActivity(Intent(this@MainActivity, AddActivity::class.java))
+                true
+            }
+            else -> false
+        }
     }
 }
