015e890e63328daad00a52187e9bcda93eaf2ff8
Gabriel Aranias <gabriel.aranias@hotmail.fr>
2022-04-23 23:12:39 +0200

    Implementation of markers on map w/ click event to display details

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/fragment/map/MapFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/fragment/map/MapFragment.kt
@@ -2,7 +2,9 @@ package com.openclassrooms.realestatemanager.ui.fragment.map
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.util.Log
@@ -13,6 +15,8 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -21,18 +25,20 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.data.model.Estate
 import com.openclassrooms.realestatemanager.databinding.FragmentMapBinding
+import com.openclassrooms.realestatemanager.ui.viewModel.EstateViewModel
 import com.openclassrooms.realestatemanager.utils.Constants
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
-
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
     private var locationPermissionGranted = false
-
     private var map: GoogleMap? = null
     private var cameraPosition: CameraPosition? = null
     private var lastKnownLocation: Location? = null
@@ -40,12 +46,16 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     // NYC coordinates
     private val defaultLocation = LatLng(40.730610, -73.935242)
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var estateViewModel: EstateViewModel
+    private lateinit var allEstates: List<Estate>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+
+        estateViewModel = ViewModelProvider(this)[EstateViewModel::class.java]
 
         permissionLauncher =
             registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
@@ -77,6 +87,40 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         updateLocationUI()
         // Get device's current location x set position of map
         getDeviceLocation()
+        // Display markers to locate estates on map
+        getEstates()
+        // Handle click on marker to display estate's details
+        map.setOnInfoWindowClickListener(this)
+    }
+
+    private fun getEstates() {
+        estateViewModel.readAllData.observe(this) { estateList ->
+            map?.clear()
+            allEstates = estateList
+            for (estate in estateList) {
+                val location = getLocationWithAddress(requireContext(), estate.address)
+                if (location != null) {
+                    val markerOptions = MarkerOptions()
+                        .position(location)
+                        .title(estate.type)
+                        .snippet(estate.address)
+                    val marker = map?.addMarker(markerOptions)
+                    marker?.tag = estateList.indexOf(estate)
+                }
+            }
+        }
+    }
+
+    private fun getLocationWithAddress(context: Context, strAddress: String): LatLng? {
+        val geocoder = Geocoder(context)
+        try {
+            val address = geocoder.getFromLocationName(strAddress, 1) ?: return null
+            val location = address.first()
+            return LatLng(location.latitude, location.longitude)
+        } catch (e: Exception) {
+            Log.w("TAG", "getLocationWithAddress: $e")
+        }
+        return null
     }
 
     @SuppressLint("MissingPermission")
@@ -154,6 +198,12 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             outState.putParcelable(Constants.CAMERA_POSITION, map.cameraPosition)
             outState.putParcelable(Constants.LOCATION, lastKnownLocation)
         }
+    }
+
+    override fun onInfoWindowClick(marker: Marker) {
+        val currentItem = allEstates[marker.tag as Int]
+        val action = MapFragmentDirections.actionMapFragmentToDetailFragment(currentItem)
+        findNavController().navigate(action)
     }
 
     override fun onDestroyView() {
